737dc019de728b0a3e8e877234e20a41ff81cab4
Vineel Sai <mail@vineelsai.com>
2023-11-12 12:08:41 +0530

    print current time

--- scripts/create_test_data.sh
+++ scripts/create_test_data.sh
@@ -17,14 +17,21 @@ mkdir -p testdir
 cd testdir
 
 for i in {1..10000} ; do
+    date
     echo "Creating dir$i"
+    date
     mkdir -p dir$i
+    date
     cd dir$i
+    date
     for j in {1..1000} ; do
+        date
         touch file$j
         echo `rand_str 100000` > file$j
+        date
     done
     cd ..
+    date
 done
 
 ls -laR
